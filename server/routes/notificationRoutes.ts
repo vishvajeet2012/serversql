71ebf8d3702fb0bb38acdc31bb73a1c04f2be6c2
vishvajeet <djritikshukla@gmail.com>
2025-10-03 12:03:45 +0530

    added notficaiton servies

--- server/routes/notificationRoutes.ts
+++ server/routes/notificationRoutes.ts
@@ -11,8 +11,8 @@ import { authenticateJWT } from "../middleware/auth";
 const router = express.Router();
 
 router.get("/getallnotification", authenticateJWT, getUserNotifications);
-router.put("/:notification_id/read", authenticateJWT, markNotificationAsRead);
+router.put("/notification_id/read", authenticateJWT, markNotificationAsRead);
 router.put("/read-all", authenticateJWT, markAllNotificationsAsRead);
-router.delete("/:notification_id", authenticateJWT, deleteNotification);
+router.delete("/notification_id", authenticateJWT, deleteNotification);
 
 export default router;
